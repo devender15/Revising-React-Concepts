4ad926c957a1bf052b0c2e232760cbc778955723
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-02 16:32:02 +0530

    learning useFetcher

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Parent from './components/Parent'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <Parent />
