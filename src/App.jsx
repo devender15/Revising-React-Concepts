a5db602a085d3a32d7a8b04bfa0bcee4affdbb17
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-04 23:18:37 +0530

    learning useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 // import Parent from './components/Parent'
-import Fetcher from "./components/fetcher/index";
+// import Fetcher from "./components/fetcher/index";
+import LearnuseCallBack from './components/callback/LearnuseCallBack'
 
 function App() {
   
   return (
-    <Fetcher />
+    <LearnuseCallBack />
   )
 }
 
