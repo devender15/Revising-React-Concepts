391b99d18a90a644e7e404a52a38662d5dbb6e02
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-04 23:47:57 +0530

    about useCallback hook

--- src/components/callback/LearnuseCallBack.jsx
+++ src/components/callback/LearnuseCallBack.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { memo, useCallback, useState, useRef } from "react";
+
+function Todo({ todos, addTodo }) {
+  const todoRef = useRef(0);
+
+  return (
+    <div className="my-6">
+      <h2 className="my-2">My Todos</h2>
+      {todos?.map((todo, idx) => {
+        return <p key={idx}>{todo}</p>;
+      })}
+      <button onClick={addTodo}>Add todo</button>
+
+      <p className="my-3">Times rendered: {todoRef.current++}</p>
+    </div>
+  );
+}
+
+Todo = memo(Todo);
 
 const LearnuseCallBack = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((count) => count + 1);
+  };
+
+  const addTodo = useCallback(() => {
+    setTodos((todos) => [...todos, "✅ new Todo"]);
+  }, [todos]);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Todo todos={todos} addTodo={addTodo} />
+      <hr />
+      <div className="flex flex-col space-y-2 items-center">
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
 
 export default LearnuseCallBack;
