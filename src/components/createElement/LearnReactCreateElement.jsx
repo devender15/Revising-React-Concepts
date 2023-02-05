a70cc9670799e1b339c0f95c51553ca235d67ef2
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-05 13:45:35 +0530

    learned about how to use React.createElement

--- src/components/createElement/LearnReactCreateElement.jsx
+++ src/components/createElement/LearnReactCreateElement.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
+import ReactDom from "react-dom";
 
 const LearnReactCreateElement = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  return React.createElement("h1", { className: "font-mono text-3xl text-yellow font-bold"}, "This is a heading!");
 }
 
+// ReactDom.render(React.createElement(LearnReactC  reateElement), document.getElementById("root"));
 export default LearnReactCreateElement;
